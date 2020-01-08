e164a9d6657152f95ce53801b9f496be98fe0453
Leicq130821 <583870243@qq.com>
2020-01-08 16:51:01 +0800

    提交

--- 元素等待.py
+++ 元素等待.py
@@ -1,14 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
-browser = webdriver.Ie()
-browser.get('http://localhost:7003/')
+from time import sleep
+driver=webdriver.Ie()
+driver.maximize_window()
+#隐式等待
+# driver.implicitly_wait(20)
+driver.get('http://localhost:9085')
 #显示等待
-element = (By.CSS_SELECTOR,'#username')
-wait = WebDriverWait(browser,5,poll_frequency=1,ignored_exceptions='等待超时')
-action = wait.until(EC.presence_of_element_located(element))
-action.send_keys('admin')
-#隐式等待—全局元素
-browser.implicitly_wait(5)
+element=(By.ID,"username1")
+wait=WebDriverWait(driver,10,poll_frequency=0.5)
+wait.until(EC.presence_of_element_located(element)).send_keys('admin')
+driver.find_element('id','password').send_keys('000000')
+driver.find_element(By.CLASS_NAME,'loginbtn').click()
+
+sleep(3)
+driver.quit()
