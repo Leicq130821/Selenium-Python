e19001aeadeab9b2c492dc108f50c4395e41ed5c
Leicq130821 <583870243@qq.com>
2020-04-15 15:08:47 +0800

    更新脚本信息

--- 基础/元素定位方法/根据元素的路径属性进行定位.py
+++ 基础/元素定位方法/根据元素的路径属性进行定位.py
@@ -1,12 +1,19 @@
-from time import sleep
 from selenium import webdriver
-browser = webdriver.Ie()
-browser.get('http://localhost:7003')
-browser.find_element_by_xpath("//input[@tabIndex='1'and @id='username']").send_keys('admin')
-browser.find_element_by_xpath("//input[starts-with(@name,'password')]").send_keys('000000')
-browser.find_element_by_xpath("//button[contains(@class,'cssLoginBtn_L1')]").click()
-sleep(5)
-browser.get('http://www.baidu.com')
-browser.find_element_by_xpath("//a[text()='新闻']").click()
-sleep(5)
+from time import sleep
+
+browser=webdriver.Ie()
+browser.maximize_window()
+browser.implicitly_wait(5)
+browser.get('http://localhost:9087')
+# 通过元素的路径信息进行定位。
+browser.find_element_by_xpath('//input[@name="username"]').send_keys('admin')
+browser.find_element_by_xpath('//*[@id="password"and@name="password"]').send_keys('000000')
+browser.find_element_by_xpath('//div/button').click()
+sleep(3)
+browser.get('http://localhost:9087')
+browser.find_element_by_xpath('//input[starts-with(@name,"username")]').send_keys('admin')
+browser.find_element_by_xpath('//input[contains(@id,"password")]').send_keys('000000')
+browser.find_element_by_xpath('//*[@onclick]').click()
+
+sleep(3)
 browser.quit()
