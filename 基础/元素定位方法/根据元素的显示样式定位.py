8d5cc2454402a10881142d6dee9ff709a7f3bf01
Leicq130821 <583870243@qq.com>
2020-04-15 15:37:47 +0800

    更新脚本信息

--- 基础/元素定位方法/根据元素的显示样式定位.py
+++ 基础/元素定位方法/根据元素的显示样式定位.py
@@ -1,25 +1,33 @@
-from time import sleep
 from selenium import webdriver
-browser = webdriver.Ie()
-browser.get('http://localhost:7003')
-#通过元素的ID属性
-browser.find_element_by_css_selector('#username').send_keys('admin')
-#通过元素的CLASS属性
-browser.find_elements_by_css_selector('.cssTextInput')[1].send_keys('000000')
-#通过元素的标签名称
+from time import sleep
+
+browser=webdriver.Ie()
+browser.maximize_window()
+browser.implicitly_wait(5)
+browser.get('http://localhost:9087')
+# 通过calss。
+browser.find_element_by_css_selector('.inputq2').send_keys('admin')
+# 通过id。
+browser.find_element_by_css_selector('#password').send_keys('000000')
+# 通过标签名称。
 browser.find_element_by_css_selector('button').click()
-browser.get('http://localhost:7003')
-#通过元素的属性
-browser.find_element_by_css_selector("[tabIndex='1']").send_keys('admin')
-browser.find_element_by_css_selector("[tabIndex='2']").send_keys('000000')
-#通过元素的路径
-browser.find_element_by_css_selector("p>button").click()
-browser.get('http://localhost:7003')
-#元素的name属性的值开头为u
-browser.find_element_by_css_selector("input[name^='u']").send_keys('admin')
-#元素的name属性的值结尾为d
-browser.find_element_by_css_selector("[name$='d']").send_keys('000000')
-#元素的type属性的值包含b
-browser.find_element_by_css_selector("[type*='b']").click()
-sleep(5)
+sleep(3)
+
+browser.get('http://localhost:9087')
+# 通过属性。
+browser.find_element_by_css_selector('[name="username"]').send_keys('admin')
+browser.find_element_by_css_selector('input[id="password"]').send_keys('000000')
+# 通过路径。
+browser.find_element_by_css_selector('div>button').click()
+sleep(3)
+
+browser.get('http://localhost:9087')
+# 属性的值开头。
+browser.find_element_by_css_selector('input[id^="user"]').send_keys('admin')
+# 属性的值结尾。
+browser.find_element_by_css_selector('input[name$="word"]').send_keys('000000')
+# 属性的值包含。
+browser.find_element_by_css_selector('[class*="ginb"]').click()
+
+sleep(3)
 browser.quit()
