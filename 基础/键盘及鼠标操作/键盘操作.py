39953c2f99554535065a34736df969b9684820e1
Leicq130821 <583870243@qq.com>
2020-04-15 17:21:30 +0800

    更新鼠标键盘操作

--- 基础/键盘及鼠标操作/键盘操作.py
+++ 基础/键盘及鼠标操作/键盘操作.py
@@ -2,13 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-browser = webdriver.Ie()
-browser.get('http://www.baidu.com/')
-element=browser.find_element_by_css_selector('#kw')
-element.send_keys('123456')
+browser=webdriver.Ie()
+browser.maximize_window()
+browser.implicitly_wait(5)
+browser.get('http://localhost:9087')
+element=browser.find_element_by_id('username')
+element.send_keys('a')
+sleep(3)
+# 键盘操作需要调用元素的send_keys()方法。
+element.send_keys(Keys.BACK_SPACE)
+element.send_keys('admin')
+# 组合键盘用逗号隔开(键盘字母需要用小写)。
 element.send_keys(Keys.CONTROL,'a')
 element.send_keys(Keys.CONTROL,'x')
+sleep(3)
 element.send_keys(Keys.CONTROL,'v')
-element.send_keys(Keys.BACK_SPACE)
-sleep(5)
+
+sleep(3)
 browser.quit()
--- 基础/键盘及鼠标操作/鼠标操作.py
+++ 基础/键盘及鼠标操作/鼠标操作.py
@@ -2,23 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
-browser = webdriver.Ie()
-browser.get('http://www.baidu.com')
-element = browser.find_element('css selector',"[name='wd']")
-element3 = browser.find_elements_by_css_selector("[name='tj_settingicon']")[1]
-element.send_keys('aaabbbccc')
+browser=webdriver.Ie()
+browser.maximize_window()
+browser.implicitly_wait(5)
+browser.get('https://www.baidu.com')
+# ActionChains(browser)实例化鼠标对象。
 action=ActionChains(browser)
-#鼠标悬停
-action.move_to_element(element3).perform()
-#鼠标右键单击
-action.context_click(element).perform()
-#鼠标左键双击
-action.double_click(element).perform()
-#鼠标拖动元素1到元素2上
-element1 = browser.find_element('css selector',"[name='wd']")
-element2 = browser.find_element('css selector',"[name='wd']")
-action.drag_and_drop(element1,element2).perform()
-#拖动元素的偏移想X多少，Y多少
-action.drag_and_drop_by_offset(element1,500,100)
-sleep(5)
+element=browser.find_element_by_xpath("//*[text()='更多产品']")
+# 移动鼠标到元素上悬停(鼠标的所有操作都是由方法perform()执行)
+action.move_to_element(element).perform()
+
+sleep(3)
 browser.quit()
