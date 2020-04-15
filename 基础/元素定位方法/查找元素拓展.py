20e730c99a08efbbd3e4c70b8db22260ee6236db
Leicq130821 <583870243@qq.com>
2020-04-15 15:49:51 +0800

    更新脚本信息

--- 基础/元素定位方法/查找元素拓展.py
+++ 基础/元素定位方法/查找元素拓展.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
-browser = webdriver.Ie()
-browser.get('http://localhost:7003/')
-#find_element_by('方法','参数')
+browser=webdriver.Ie()
+browser.maximize_window()
+browser.implicitly_wait(5)
+browser.get('http://localhost:9087')
+# find_element('方法名','参数值')。
 browser.find_element('id','username').send_keys('admin')
-browser.find_element('name','password').send_keys('000000')
+browser.find_element('xpath','//input[@name="password"]').send_keys('000000')
 browser.find_element('css selector','button').click()
-browser.back()
-browser.get('http://localhost:7003/')
-#通By类：find_element(By.方法,'参数')
-browser.find_element(By.ID,'username').send_keys('admin')
-browser.find_element(By.NAME,'password').send_keys('000000')
-browser.find_element(By.CSS_SELECTOR,'button').click()
+sleep(3)
+
+browser.get('http://localhost:9087')
+# find_element(By.方法大写,'参数值')。
+browser.find_element(By.NAME,'username').send_keys('admin')
+browser.find_element(By.CSS_SELECTOR,'[id="password"]').send_keys('000000')
+browser.find_element(By.TAG_NAME,'button').click()
+
+sleep(3)
 browser.quit()
