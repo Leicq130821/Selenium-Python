3d63e355cc0e0ef56ec7ae744deb081d7c7914ce
Leicq130821 <583870243@qq.com>
2020-04-15 19:59:14 +0800

    提交下拉框操作与弹出框操作

--- 基础/下拉框弹出框滚动条/下拉框操作.py
+++ 基础/下拉框弹出框滚动条/下拉框操作.py
@@ -1,28 +1,36 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
+from time import sleep
 
-browser = webdriver.Ie()
+browser=webdriver.Ie()
 browser.maximize_window()
 browser.implicitly_wait(5)
-browser.get('http://localhost/biz/user-login.html')
-browser.find_element('css selector','#account').send_keys('admin')
-browser.find_element('css selector',"input[name='password']").send_keys('000000')
-browser.find_element('css selector','#submit').click()
-browser.find_element_by_link_text('测试').click()
-browser.find_element_by_link_text('Bug').click()
-browser.find_element_by_link_text('提Bug').click()
-selectelement=browser.find_element('id','type')
-action = Select(selectelement)
-#通过索引来查找下拉选项
-action.select_by_index(2)
-# 通过下拉选项的value值
-action.select_by_value('性能问题')
-# 通过下拉选项的显示文本
-action.select_by_visible_text('代码问题')
-
-browser.find_element_by_class_name('dropdown-toggle').click()
-browser.find_element_by_link_text('签退').click()
+browser.get('http://localhost:9087')
+browser.find_element_by_id('username').send_keys('admin')
+browser.find_element_by_id('password').send_keys('000000')
+browser.find_element_by_tag_name('button').click()
+sleep(3)
+browser.switch_to.frame('topFrame')
+browser.find_element_by_id('mmenu8').click()
+browser.switch_to.default_content()
+browser.switch_to.frame('leftFrame')
+browser.switch_to.frame('leftiframe')
+browser.find_element_by_id('itemTextLink5').click()
+browser.find_element_by_id('itemTextLink9').click()
+browser.switch_to.default_content()
+browser.switch_to.frame('content')
+browser.find_element_by_id('btnNew').click()
+handles=browser.window_handles
+browser.switch_to.window(handles[-1])
+element=browser.find_element_by_name('proj_mem_role')
+# options=Select(element):实例化下拉框对象。
+options=Select(element)
+# 通过下拉选项的索引选择下拉选项。
+options.select_by_index(1)
+# 通过下拉选项的value值选择下拉选项。
+options.select_by_value('02')
+# 通过下拉选项显示的文本信息选择下拉选项。
+options.select_by_visible_text('项目分管领导')
 
 sleep(3)
 browser.quit()
--- 基础/下拉框弹出框滚动条/弹出框处理.py
+++ 基础/下拉框弹出框滚动条/弹出框处理.py
@@ -1,20 +1,26 @@
-from time import sleep
 from selenium import webdriver
+from time import sleep
 
-browser = webdriver.Ie()
+browser=webdriver.Ie()
 browser.maximize_window()
 browser.implicitly_wait(5)
-browser.get('http://localhost/biz/user-login.html')
-browser.find_element('css selector','#account').send_keys('admin')
-browser.find_element('css selector',"input[name='password']").send_keys('000000')
-browser.find_element('css selector','#submit').click()
-tanchukuang = browser.switch_to.alert
-#弹出框文本
-print(tanchukuang.text)
-#接受弹出框
-tanchukuang.accept()
-#拒绝/取消弹出框
-tanchukuang.dismiss()
+browser.get('http://localhost:9087')
+browser.find_element_by_id('username').send_keys('admin')
+browser.find_element_by_id('password').send_keys('000000')
+browser.find_element_by_tag_name('button').click()
+sleep(3)
+browser.switch_to.frame('topFrame')
+browser.find_element_by_link_text('退出系统').click()
+# 切换到弹出框。
+alert=browser.switch_to.alert
+# 获取弹出框的文本信息。
+print(alert.text)
+# 拒绝弹出框信息。
+alert.dismiss()
+browser.find_element_by_link_text('退出系统').click()
+alert=browser.switch_to.alert
+# 接受弹出框信息。
+alert.accept()
 
 sleep(3)
 browser.quit()
