04e72ffd5c389e5376dbcf3b2c76e0266c37b44f
Leicq130821 <583870243@qq.com>
2020-04-17 11:10:51 +0800

    提交jQuery定位

--- 基础/TestCase/testcase2.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class number2(unittest.TestCase):
-    def test2(self):
-        print('第二个用例被执行')
-if __name__ == '__main__':
-    unittest.main()
-
--- 基础/TestCase/testcase3.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-class number3(unittest.TestCase):
-    def test3(self):
-        print('第三个用例被执行')
-if __name__ == '__main__':
-    unittest.main()
-
