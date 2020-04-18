a95b2d5be066d7cec289bfe3f607abbeeb57447c
Leicq130821 <583870243@qq.com>
2020-04-18 14:56:23 +0800

    提交

--- 基础/UnitTest框架/Html测试报告.py
+++ 基础/UnitTest框架/Html测试报告.py
@@ -1,13 +1,10 @@
 from HTMLTestRunner import HTMLTestRunner
-import unittest
 from datetime import datetime
+import unittest
 
 if __name__ == '__main__':
-    # 添加测试套件
+    filename=r'D:\自动化测试\%s'%datetime.now().strftime('%Y年%m月%d日 %H点%M分%S秒')+'Report.html'
     suite=unittest.defaultTestLoader.discover('./Case',pattern='case*.py')
-    #定义报告的文件目录
-    file_name=r'D:\自动化测试\%sReport.html'%datetime.now().strftime('%Y年%m月%d日 %H点%M分%S秒')
-    #写入报告
-    with open(file_name,"wb") as f:
-        runner=HTMLTestRunner(stream=f,title='测试报告',description='测试内容')
+    with open(filename,'wb') as file:
+        runner=HTMLTestRunner(stream=file,title='测试报告',description='测试内容')
         runner.run(suite)
