b6212e1793dc538f8327148ee710b4bf6132b841
Leicq130821 <583870243@qq.com>
2020-04-18 15:28:05 +0800

    提交

--- 基础/拓展实例化360浏览器/实例化360浏览器.py
+++ 基础/拓展实例化360浏览器/实例化360浏览器.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 
+# 指定浏览器的运行内核。
 options = webdriver.ChromeOptions()
+# 指定启动程序。
 options.binary_location=r'E:\常用软件\360浏览器\360se6\Application\360se.exe'
+# 实例化浏览器指定驱动。
 driver = webdriver.Chrome(r'E:\WebDriver\chromedriver_v2.36.exe', options=options)
-driver.get('http://127.0.0.1/biz/user-login-L2Jpei8=.html')
-driver.find_element_by_id('account').send_keys('admin')
-driver.find_element_by_name('password').send_keys('000000')
-driver.find_element_by_id('submit').click()
