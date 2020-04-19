93792779087bd2dc713c0ff8a2b3c673053625d3
Leicq130821 <583870243@qq.com>
2020-04-19 16:47:44 +0800

    提交

--- 基础/元素定位方法/基本定位方法的其他写法.py
+++ 基础/元素定位方法/基本定位方法的其他写法.py
@@ -17,6 +17,15 @@ browser.get('http://localhost:9087')
 browser.find_element(By.NAME,'username').send_keys('admin')
 browser.find_element(By.CSS_SELECTOR,'[id="password"]').send_keys('000000')
 browser.find_element(By.TAG_NAME,'button').click()
+sleep(3)
+# 通过父元素调用查找元素的方法定位子元素。
+browser.get('http://localhost:9087')
+browser.find_element_by_id('username').send_keys('admin')
+browser.find_element_by_id('password').send_keys('000000')
+# 父元素。
+login=browser.find_element_by_class_name('logindiv')
+# 子元素。
+login.find_element_by_tag_name('button').click()
 
 sleep(3)
 browser.quit()
