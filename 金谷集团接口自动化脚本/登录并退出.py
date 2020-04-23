1805536c7d2c04e19e25f59743fd2d998f1ac253
Leicq130821 <583870243@qq.com>
2020-04-23 19:31:57 +0800

    提交

--- 金谷集团接口自动化脚本/登录并退出.py
+++ 金谷集团接口自动化脚本/登录并退出.py
@@ -1,9 +1,13 @@
 import sys,os
+path = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(path)[0]
+sys.path.append(rootPath)
+
 from 金谷集团接口自动化脚本.封装执行脚本 import Test
 from 金谷集团接口自动化脚本.封装数据库返回数据方法 import Data
 
 if __name__ == '__main__':
-    rootPath = os.path.split(os.getcwd())[0]
-    sys.path.append(rootPath)
+    with open(r'D:\自动化测试\test.txt','w') as file:
+        file.write(rootPath)
     DATA=Data().data(2)
     Test().test(*DATA)
