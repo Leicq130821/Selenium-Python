ec653bc5c1f8ad47b9ca62336e4bfd5fb8d70cf2
Leicq130821 <583870243@qq.com>
2020-04-23 10:05:10 +0800

    提交

--- 金谷集团接口自动化脚本/登录并退出.py
+++ 金谷集团接口自动化脚本/登录并退出.py
@@ -1,6 +1,9 @@
+import sys,os
 from 金谷集团接口自动化脚本.封装执行脚本 import Test
 from 金谷集团接口自动化脚本.封装数据库返回数据方法 import Data
 
 if __name__ == '__main__':
+    rootPath = os.path.split(os.getcwd())[0]
+    sys.path.append(rootPath)
     DATA=Data().data(2)
     Test().test(*DATA)
